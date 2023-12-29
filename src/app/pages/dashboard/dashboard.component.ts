e7bf8a694647cd3c1973d9861b588357ea587ec2
luisfer2818 <luisfernando1011@hotmail.com>
2023-12-29 19:15:47 -0300

    add grafico chart.js

--- src/app/pages/dashboard/dashboard.component.ts
+++ src/app/pages/dashboard/dashboard.component.ts
@@ -1,7 +1,63 @@
+import { LiveAnnouncer } from '@angular/cdk/a11y';
 import { Component, OnInit, ViewChild } from '@angular/core';
 import { MatAccordion } from '@angular/material/expansion';
-// const { Chart } = await import('chart.js');
+import { MatPaginator } from '@angular/material/paginator';
+import { MatSort, Sort } from '@angular/material/sort';
+import { MatTableDataSource } from '@angular/material/table';
 import Chart from 'chart.js/auto';
+
+export interface PeriodicElement {
+  id: number;
+  titulo: string;
+  departamento: string;
+  data: string;
+  status: string;
+}
+
+const ELEMENT_DATA: PeriodicElement[] = [
+  {
+    id: 1,
+    titulo: 'Site lento para acessar',
+    departamento: 'Vendas',
+    data: '22/12/2023',
+    status: 'aberto',
+  },
+  {
+    id: 2,
+    titulo: 'Meu pc tá ruim',
+    departamento: 'Tecnologia',
+    data: '24/12/2023',
+    status: 'em andamento',
+  },
+  {
+    id: 3,
+    titulo: 'Pc sem acesso a internet',
+    departamento: 'Tecnologia',
+    data: '26/12/2023',
+    status: 'concluido',
+  },
+  {
+    id: 4,
+    titulo: 'Notebook sem som',
+    departamento: 'Financeiro',
+    data: '29/12/2023',
+    status: 'concluido',
+  },
+  {
+    id: 5,
+    titulo: 'Instalação de software - Node.js(16.6)',
+    departamento: 'Tecnologia',
+    data: '30/12/2023',
+    status: 'em andamento',
+  },
+  {
+    id: 6,
+    titulo: 'Pc com lentidão',
+    departamento: 'Financeiro',
+    data: '28/12/2022',
+    status: 'aberto',
+  },
+];
 
 @Component({
   selector: 'app-dashboard',
@@ -10,18 +66,52 @@ import Chart from 'chart.js/auto';
 })
 export class DashboardComponent implements OnInit {
   @ViewChild(MatAccordion) accordion!: MatAccordion;
-  constructor() {
+  @ViewChild(MatSort) sort!: MatSort;
+  @ViewChild(MatPaginator) paginator!: MatPaginator;
+
+  constructor(private _liveAnnouncer: LiveAnnouncer) {
     let panelOpenState = false;
   }
+  displayedColumns: string[] = [
+    'id',
+    'titulo',
+    'departamento',
+    'data',
+    'status',
+  ];
+  dataSource = new MatTableDataSource(ELEMENT_DATA);
 
-  ngOnInit(): void {
-    // let ctx = document.getElementById('MyChart');
-    const chart = new Chart('myChart', {
+  ngOnInit() {
+    new Chart(document.getElementById('pie-chart') as HTMLCanvasElement, {
       type: 'pie',
       data: {
         labels: ['Em aberto', 'Em andamento', 'Concluidas'],
-        datasets: [],
+        datasets: [
+          {
+            label: 'Tickets',
+            data: [7, 4, 43],
+            backgroundColor: [
+              'rgb(255, 57, 57)',
+              'rgb(54, 162, 235)',
+              'rgb(58, 238, 53)',
+            ],
+            hoverOffset: 4,
+          },
+        ],
       },
     });
+  }
+
+  ngAfterViewInit() {
+    this.dataSource.sort = this.sort;
+    this.dataSource.paginator = this.paginator;
+  }
+
+  announceSortChange(sortState: Sort) {
+    if (sortState.direction) {
+      this._liveAnnouncer.announce(`Sorted ${sortState.direction}ending`);
+    } else {
+      this._liveAnnouncer.announce('Sorting cleared');
+    }
   }
 }
--- src/app/pages/dashboard/dashboard.module.ts
+++ src/app/pages/dashboard/dashboard.module.ts
@@ -6,11 +6,16 @@ import { MatButtonModule } from '@angular/material/button';
 import { MatCardModule } from '@angular/material/card';
 import { MatNativeDateModule } from '@angular/material/core';
 import { MatDatepickerModule } from '@angular/material/datepicker';
+import { MatDividerModule } from '@angular/material/divider';
 import { MatExpansionModule } from '@angular/material/expansion';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatGridListModule } from '@angular/material/grid-list';
 import { MatIconModule } from '@angular/material/icon';
 import { MatInputModule } from '@angular/material/input';
+import { MatListModule } from '@angular/material/list';
+import { MatPaginatorModule } from '@angular/material/paginator';
+import { MatSortModule } from '@angular/material/sort';
+import { MatTableModule } from '@angular/material/table';
 import { DashboardMaterialModule } from '@app/shared/materials/dashboard-mat.module';
 import { SharedModule } from '@app/shared/shared.module';
 import { DashboardRoutingModule } from './dashboard-routing.module';
@@ -33,6 +38,11 @@ import { DashboardComponent } from './dashboard.component';
     MatCardModule,
     MatIconModule,
     MatBadgeModule,
+    MatDividerModule,
+    MatListModule,
+    MatTableModule,
+    MatSortModule,
+    MatPaginatorModule,
   ],
 })
 export class DashboardModule {}
